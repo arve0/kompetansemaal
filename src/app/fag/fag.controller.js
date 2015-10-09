bfa6d974682bacb3de2c97c531f489336ac71cc7
arve0 <arve.seljebu@gmail.com>
2015-10-09 19:13:53 +0200

    fag: "done"

--- src/app/fag/fag.controller.js
+++ src/app/fag/fag.controller.js
@@ -6,14 +6,31 @@
     .controller('FagController', FagController);
 
   /** @ngInject */
-  function FagController($stateParams, udir, mainState) {
+  function FagController($stateParams, udir, mainState, $log) {
     var fag = this;  // view model
     fag.state = mainState;  // access to language
 
     fag.params = $stateParams;
     fag.data = udir.getREST(fag.params.id).then(function(data){
       fag.data = data;
+
+      for (var i=0, l=fag.data.opplaeringsfag.length; i<l; ++i){
+        getLaereplanReferanse(fag.data.opplaeringsfag[i]);
+      }
     });
+
+
+    function getLaereplanReferanse(item){
+      udir.getREST(item.kode)
+        .then(function(opplaeringsfag){
+          if (!('laereplanreferanse' in opplaeringsfag)) {
+            $log.error('Referanse til læreplan ikke funnet for '+ item.kode);
+            return;
+          }
+          item.laereplanreferanse = opplaeringsfag.laereplanreferanse;
+        });
+    }
+
 
   }
 })();
